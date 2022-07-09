0fd025c495002a4441054664ce766dd0b1bac783
baiqiletter <baiqiletter@gmail.com>
2022-07-09 21:45:19 +0800

    修改默认的数据库配置

--- server/settings.py
+++ server/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'notes',
-        'USER': 'root',
-        'PASSWORD': 'qweasd123456.',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'notes',
+        # 'USER': 'root',
+        # 'PASSWORD': 'qweasd123456.',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
